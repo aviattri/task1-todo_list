b74cb5c37caf15d6840bd987a65df8a45f73cf67
aviattri <37525378+aviattri@users.noreply.github.com>
2018-12-29 01:31:10 +1100

    More detialed step by step comments, somewhow missed by earlier commit

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,74 @@
+//syntax to import the react lib
 import React, { Component } from 'react';
-import './App.css';
+//import TodoItems from "./TodoItems";
+//this is done to get the react object from react module
+
+/** Trivia
+ * A module is a self contained unit that can expose assests to other modules 
+ *  or
+ *  can acquire assest using import  
+ */
  
-class App extends Component {
+class TodoList extends Component {
+  constructor(props){
+   super(props);
+   //binding the even handlers
+   this.state = {
+     //for manting the state of the items
+     items: []
+   }
+   this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e){
+   if(this._inputElement.value !==""){
+
+    var newItem = {
+       text: this._inputElement.value,
+       //we need a key value here becuase we are dealing with multiple items
+       key: Date.now()
+    };
+
+    this.setState((prevState) => {
+      // the previous state argument gives the value of the state object just before
+        return{
+          items:prevState.items.concat(newItem)
+          //concat is to modify the array by adding in the new item
+        }
+      }
+     );
+
+    this._inputElement.value = "";
+    //this is to clear out the form once the value has been submitted
+    }
+    /** 
+     * Now to make sure that the things are working fine
+     * we need to log it to the console 
+     */
+    console.log(this.state.items);
+
+    /** 
+     * To overcume the default behaviour of the form of refreshing the webpage everytime when 
+     * something is submiited to it, we need to use prevent default
+     */
+    e.preventDefault();
+  }
+
   render(){
     return(
-      <div id="cont1">
-        <center>
-        <input type="text" placeholder="whats in ur mind"
-          className="form-control"/>
-
-        <button className="btn btn-danger">add item </button>
-        </center>
+      <div className="todoListMain" id="cont1">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input  ref={(a) => this._inputElement = a}
+            
+            placeholder="whats in ur mind"/>
+            <button type="submit">add item </button>
+          </form>  
+        </div>
+        {/*<TodoItems entries={this.state.items}/> */}
       </div>
-    );
+    );   
   }
 }
 
-export default App;
+export default TodoList; //expose the todolist component to other modules
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@
  //this is telling the bundeling tool to take all the
  // peice of the app and put it togeter in one css html bundle
 
- var destination = document.querySelector("#container");
+ var destination = document.querySelector("#root");
  ReactDOM.render(
      <div>
         <TodoList/>
      </div>, destination
  );
+
+
